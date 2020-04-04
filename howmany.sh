6749dac5bde65052e0d8b7f31a9a364d4c720812
Kai Carver <kaicarver@gmail.com>
2020-04-04 10:55:07 +0200

    send output to clipboard works

--- howmany.sh
+++ howmany.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Keep track of how much I commit in the day
 
 # TODO: optional more detail about each commit: what files modified, how many lines...
@@ -8,6 +8,7 @@ ROOT=~
 WDIR=~/tools
 DDIR=$WDIR/data
 FILE=$DDIR/commits.`date +%Y-%m-%d`.txt
+TEMP=temp
 
 # list the day's logs for 1 git project into a file
 daylog() {
@@ -19,14 +20,14 @@ daylog() {
 }
 
 # list the day's logs for a list of git projects into a file
-rm -f $FILE
+rm -f $FILE $TEMP
 for repo in svelte-demo svelte-demo-debug lapeste sovelte blog yuansu-react git-sum jamstack-comments-engine vizhubbarchart todo tools
 do
     cd $ROOT/$repo; daylog
 done
 
 # print all the day's logs, in order
-cat $FILE | cut -c 6- | sed 's/^ 0/  /' | sort
+cat $FILE | cut -c 6- | sed 's/^ 0/  /' | sort | tee -a $TEMP
 
 # overall summary of the day's activity
 echo did `cat $FILE | sort | wc -l` commits \
@@ -34,12 +35,17 @@ echo did `cat $FILE | sort | wc -l` commits \
      at `cat $FILE | cut -d' ' -f 2 | cut -d: -f 1 | sort -u | wc -l` \
      ≠ hours \
      and `cat $FILE | cut -d' ' -f 2 | cut -c -4 | sed 's/:[0-2]/↑/' | sed 's/:[3-5]/↓/' | sort -u | wc -l` \
-     ≠ half-hours of the day
+     ≠ half-hours of the day \
+     | tee -a $TEMP
 
 # repos
-echo " "`cat $FILE | cut -d\> -f 2- | sort -u | tr -s '\n' ' '`
+echo " "`cat $FILE | cut -d\> -f 2- | sort -u | tr -s '\n' ' '` | tee -a $TEMP
 # hours
-echo " "`cat $FILE | cut -d' ' -f 2 | cut -d: -f 1 | sort -u | tr -s '\n' ' '`
+echo " "`cat $FILE | cut -d' ' -f 2 | cut -d: -f 1 | sort -u | tr -s '\n' ' '` | tee -a $TEMP
 # half hours
-echo " "`cat $FILE | cut -d' ' -f 2 | cut -c -4 | sed 's/:[0-2]/↑/' | sed 's/:[3-5]/↓/' | sort -u | tr -s '\n' ' '`
-echo It\'s `date +%H:%M` | tee /dev/fd/2 | clip.exe
+echo " "`cat $FILE | cut -d' ' -f 2 | cut -c -4 | sed 's/:[0-2]/↑/' | sed 's/:[3-5]/↓/' | sort -u | tr -s '\n' ' '` | tee -a $TEMP
+echo It\'s `date +%H:%M` | tee -a $TEMP
+cat $TEMP | clip.exe
+
+# don't need this, but it's a clever bash trick to redirect stdout to multiple processes
+# cat $TEMP | tee >(clip.exe) >(cmd2) >(cmd3) ...
