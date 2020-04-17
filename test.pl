acd7b4e65ca0b9b6ade5aebb3e08a524f44c8b15
Kai Carver <kaicarver@gmail.com>
2020-04-17 14:01:13 +0200

    a solution to problem 3 levels deep in distraction

--- test.pl
+++ test.pl
@@ -1,6 +1,11 @@
 # this fails, why
 #   $ echo "22↑ 22→ " | perl -pe 's/[↑]/X/g'
 #   22XXX 22XX 
+# this no better
+#   $ echo "22↑ 22→ " | perl -Mutf8 -pe 's/[↑]/X/g'
+#   22↑ 22→
+use utf8;
+use open ':std', ':encoding(UTF-8)'; # avoid "Wide character in print at test.pl line 13."
 
 $_ = "22↑ 22→ \n";
 
