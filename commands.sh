7d67ff1ce57ed6376c146da12099fde195883279
Kai Carver <kaicarver@gmail.com>
2020-04-28 21:38:32 +0200

    some extra commands for managing the various repos

---
+++ commands.sh
@@ -0,0 +1,10 @@
+# Some commands I use from my history and don't want to lose
+
+cd ~
+
+# status about all projects
+git-sum2/git-summary -lq
+# more status about list of projects
+for r in blog leaflet clock geotools bootcamp_python lapeste yuansu-react uketabs tools; do echo $r : `git -C $r status | grep ahead`; done
+# push it
+for r in lapeste uketabs tools; do echo $r : `git -C $r push`; done
