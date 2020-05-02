182079874cca8f27ba1ce92c4ef4a9b002a92774
Kai Carver <kaicarver@gmail.com>
2020-05-02 22:59:38 +0200

    yup just 2 new repos; bedtime

--- howmany.sh
+++ howmany.sh
@@ -13,7 +13,8 @@
 
 PROJECTS="svelte-demo svelte-demo-debug lapeste sovelte blog \
     yuansu-react git-sum jamstack-comments-engine vizhubbarchart \
-    todo tools uketabs clock bootcamp_python_kai geotools leaflet"
+    todo tools uketabs clock bootcamp_python_kai geotools leaflet \
+    sedfugit laravel"
 ROOT=~
 WDIR=~/tools
 DDIR=$WDIR/data
