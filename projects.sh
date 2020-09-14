5ffbc8ca509a41bb51954df01dad187de141828b
Kai Carver <kaicarver@gmail.com>
2020-09-14 12:55:23 +0200

    add kaicarver.github.io as a new project

--- projects.sh
+++ projects.sh
@@ -5,6 +5,6 @@ PROJECTS="svelte-demo svelte-demo-debug lapeste sovelte blog \
     yuansu-react git-sum jamstack-comments-engine vizhubbarchart \
     todo tools uketabs clock bootcamp_python_kai geotools leaflet \
     sedfugit laravel zi hbd nextjs-blog csf amplify/postagram \
-    textapi ytplayer worldword"
+    textapi ytplayer worldword kaicarver.github.io"
 
 PROJECTS=$(echo $PROJECTS | tr " " "\n" | sort)
