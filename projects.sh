e69e90fc987ea23581b5328ab7614dfb8fe553c9
Kai Carver <kaicarver@gmail.com>
2020-09-14 22:19:55 +0200

    another fast.ai project

--- projects.sh
+++ projects.sh
@@ -5,6 +5,6 @@ PROJECTS="svelte-demo svelte-demo-debug lapeste sovelte blog \
     yuansu-react git-sum jamstack-comments-engine vizhubbarchart \
     todo tools uketabs clock bootcamp_python_kai geotools leaflet \
     sedfugit laravel zi hbd nextjs-blog csf amplify/postagram \
-    textapi ytplayer worldword kaicarver.github.io"
+    textapi ytplayer worldword kaicarver.github.io fastai"
 
 PROJECTS=$(echo $PROJECTS | tr " " "\n" | sort)
