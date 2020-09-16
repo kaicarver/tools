adc31ac913ca8d255ed5bd9e138f694617e4fb04
Kai Carver <kaicarver@gmail.com>
2020-09-16 15:09:51 +0200

    add fastaixp repo for fast.ai experiments

--- projects.sh
+++ projects.sh
@@ -5,6 +5,6 @@ PROJECTS="svelte-demo svelte-demo-debug lapeste sovelte blog \
     yuansu-react git-sum jamstack-comments-engine vizhubbarchart \
     todo tools uketabs clock bootcamp_python_kai geotools leaflet \
     sedfugit laravel zi hbd nextjs-blog csf amplify/postagram \
-    textapi ytplayer worldword kaicarver.github.io fastai"
+    textapi ytplayer worldword kaicarver.github.io fastai fastaixp"
 
 PROJECTS=$(echo $PROJECTS | tr " " "\n" | sort)
